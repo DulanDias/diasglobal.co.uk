7952eb906d77f87f8b97502e6b0424a58cfb1479
dulandias-tellme <dulan.dias@tell-me.ai>
2025-09-20 12:44:27 +0100

    next js deployment

---
+++ _next/static/kYC7jUqkqW_zwMxnQiZ7u/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/204a44a95c1a09e2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
